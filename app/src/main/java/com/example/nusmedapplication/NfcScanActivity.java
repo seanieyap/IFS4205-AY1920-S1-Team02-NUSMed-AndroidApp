946727ec219451e189ad794343818839814157c7
dingheng4448 <ldh_1295@hotmail.com>
2019-09-08 04:08:27 +0800

    Fix NFC scan issues

--- app/src/main/java/com/example/nusmedapplication/NfcScanActivity.java
+++ app/src/main/java/com/example/nusmedapplication/NfcScanActivity.java
@@ -6,11 +6,17 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class NfcScanActivity extends AppCompatActivity {
+
+    private static final String TAG = "NfcScanActivity";
 
     private TextView mText;
     private NfcAdapter nfcAdapter;
@@ -21,12 +27,11 @@ public class NfcScanActivity extends AppCompatActivity {
         super.onCreate(savedState);
         setContentView(R.layout.activity_nfc_scan);
 
-        mText = (TextView) findViewById(R.id.text);
-        mText.setText("Scan a tag");
+        mText = findViewById(R.id.text);
+        mText.setText(R.string.scan_user_tag_helptext);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-        // TODO: handle if smartphone has nfc and ensure app has nfc permissions
         // Check if the smartphone has NFC
         if (nfcAdapter == null) {
             Toast.makeText(this, "NFC not supported", Toast.LENGTH_LONG).show();
@@ -35,17 +40,15 @@ public class NfcScanActivity extends AppCompatActivity {
 
         // Check if NFC is enabled
         if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, "Enable NFC before using the app",
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Enable NFC before using the app", Toast.LENGTH_LONG).show();
+            finish();
         }
 
-        Toast.makeText(this, "NFC enabled",
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "NFC enabled", Toast.LENGTH_LONG).show();
 
         Intent nfcIntent = new Intent(this, getClass());
         nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        nfcPendingIntent =
-                PendingIntent.getActivity(this, 0, nfcIntent, 0);
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
     }
 
     @Override
@@ -58,14 +61,49 @@ public class NfcScanActivity extends AppCompatActivity {
     @Override
     public void onNewIntent(Intent intent) {
         Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        Toast.makeText(getBaseContext(), tagFromIntent.toString(), Toast.LENGTH_LONG).show();
-        mText.setText("Tag ID: " + tagFromIntent.getId());
+        MifareUltralight nfcTag = MifareUltralight.get(tagFromIntent);
+
+        try {
+            nfcTag.connect();
+
+            // Read from page 6 of the NFC tag as the tag's unique ID is stored there
+            byte[] uniqueIdBytes = nfcTag.readPages(6);
+
+            mText.setText("Tag ID: " + encodeHexString(uniqueIdBytes));
+            Toast.makeText(getBaseContext(), "Scan successful!", Toast.LENGTH_LONG).show();
+
+            //TODO: Retrieve device ID, send device ID + tag ID to server to authenticate user, go to next activity
+
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while writing MifareUltralight...", e);
+        } finally {
+            try {
+                nfcTag.close();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException while closing MifareUltralight...", e);
+            }
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
         if (nfcAdapter != null) nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    public String byteToHex(byte num) {
+        char[] hexDigits = new char[2];
+        hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+        hexDigits[1] = Character.forDigit((num & 0xF), 16);
+        return new String(hexDigits);
+    }
+
+    public String encodeHexString(byte[] byteArray) {
+        StringBuilder hexStringBuilder = new StringBuilder();
+        for (byte byteToConvert : byteArray) {
+            hexStringBuilder.append(byteToHex(byteToConvert));
+        }
+        return hexStringBuilder.toString();
     }
 
     // TODO: disable back navigation
